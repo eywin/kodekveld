692779d57322cde1091518eb25e7be5816754e55
oyv <oyvind@lovskeid.no>
2024-06-26 19:03:19 +0200

    add text

--- 2024-06-26/Oppgave1/Oppgave1.cs
+++ 2024-06-26/Oppgave1/Oppgave1.cs
@@ -1,5 +1,15 @@
 namespace _2024_06_26.Oppgave1;
 
+
+/*
+   a) Skriv et program som ber brukeren om å skrive inn favorittfargen
+   sin i terminalen og lagre svaret i en variabel farge.
+
+   b) Utvid programmet slik at hvis brukeren svarer "gul", "oransje" 
+   eller "grønn", skal programmet skrive ut navnet på en frukt som har den fargen.
+ */
+
+
 public class Oppgave1
 {
     
