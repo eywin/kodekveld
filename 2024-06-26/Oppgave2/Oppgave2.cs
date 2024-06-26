4d0c867d613840c86daf419f75dd2210f0ac1349
oyv <oyvind@lovskeid.no>
2024-06-26 19:44:45 +0200

    oppgave 2

---
+++ 2024-06-26/Oppgave2/Oppgave2.cs
@@ -0,0 +1,22 @@
+namespace _2024_06_26.Oppgave2;
+
+
+/*
+Oppgave:
+https://trix.ifi.uio.no/assignments/424
+
+I det fiktive landet Ruritania er skattereglene slik at hvis en person har inntekt < 10000, 
+så betaler man 10% skatt på hele inntekten, og hvis inntekten >= 10000, 
+så betaler man 10% skatt på de første 10000 kronene og 30% skatt på resten av inntekten. 
+Lag et program som regner ut og skriver ut hvor mange kroner som skal betales i skatt gitt inntekten. 
+Programmet skal lese inntekten (som antas å være et flyttall) fra terminal.
+
+
+*/
+public class Oppgave2
+{
+    public static void Oppgave()
+    {
+        
+    }
+}
